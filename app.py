95a74b958036102ccbdfb967a1ab8940dace39bb
Kevinz297 <Kevinz297@nycstudents.net>
2026-02-04 08:45:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-def print_values(values):
-    print(values[x])
-    print(values[y])
-    print(values[z])
-    for i in values[x:y:z]:
-        print(i)
-values = [0, 1, 2, 3, 4, 5, 6]
+def discount(age, is_number, is_resident):
+    if age >= 12 and age <= 65:
+        print("Not eligible for discount")
+    elif age <=12 or age>=65:
+        print("Eligible for discount")
+        return
 
-x = 5
-y = 1
-z = 2
+    elif is_number or is_resident == True:
+        print("Eligible for full discount")
+    elif is_number == True:
+        print("Eligible for discount")
+    elif is_resident == True:
+        print("Eligible for discount")
+    else:
+        print("Not eligible for discount")
+        return
 
-print_values(values)
-print("Done")
+age = 30
+is_number = True
+is_resident = False
