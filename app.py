e3915de142eccfbb3cc1654a055afad0b6508994
Kevinz297 <Kevinz297@nycstudents.net>
2026-02-05 09:23:18 -0500

    Vacation discount

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 def discount(age, is_number, is_resident):
-    if age >= 12 or age <= 65:
+    age = int(input("How old are you?"))
+    if age >= 12 and age <= 65:
         print("Not eligible for discount")
-    elif age <=12 or age>=65:
-        print("Eligible for 20% discount")
-    elif is_number or is_resident == True:
-        print("Eligible for 30% discount")
+    elif age <= 12 or age >= 65:
+        print("Eligible for $20 discount")
+        return
+    is_number = input("Are you a member of the club? (yes/no) ")
+    is_resident = input("Are you a resident? (yes/no) ")
+    if is_number == "yes" or is_resident == "yes":
+        print("Eligible for $30 discount")
     else:
         print("Not eligible for discount")
         return
 
 age = 30
-is_number = True
-is_resident = False
+is_number = "yes"
+is_resident = "no"
 
 discount(age, is_number, is_resident)
